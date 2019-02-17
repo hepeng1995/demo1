eedb417149fe632279d88b4a80711c6daba5d2c3
何鹏 <41983712+hepeng1995@users.noreply.github.com>
2019-02-17 15:24:11 +0800

    Update MyInvocationHandler.java

--- src/reflect/MyInvocationHandler.java
+++ src/reflect/MyInvocationHandler.java
@@ -12,7 +12,7 @@ public class MyInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method,Object[] args)throws Throwable{
         System.out.println("我被代理了");
         method.invoke(target,args);   //执行被代理对象target的方法
-        System.out.println("我被代理完成了");
+        System.out.println("我被代理完成");
         return null;
     }
 }
